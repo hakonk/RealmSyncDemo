f001e75b61c3d54965d8ab271a38ebf292edd601
Håkon Knutzen <hakn@netlight.com>
2017-01-29 20:38:32 +0100

    Initial Commit

---
+++ RealmChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmChat
+//
+//  Created by Håkon Knutzen on 29/01/2017.
+//  Copyright © 2017 Håkon Knutzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
